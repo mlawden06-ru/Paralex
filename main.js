422efd525212ba47d369d13eb83adaabee65679c
Paralex <hfhfdhjdfh@gmail.com>
2025-12-03 15:53:55 +0500

    Paralexent

---
+++ main.js
@@ -0,0 +1,15 @@
+let cloud1 = document.querySelector('.cloud1')
+let cloud2 = document.querySelector('.cloud2')
+let cloud3 = document.querySelector('.cloud3')
+let boat = document.querySelector('.boat')
+
+window.addEventListener('scroll', () => {
+
+let windowY = window.scrollY
+cloud1.style.transform = `translate(-${windowY}px)`
+cloud2.style.transform = `translate(${windowY}px)`
+cloud3.style.transform = `translate(${windowY}px)`
+boat.style.transform = `translate(-${windowY}px)`
+
+
+})
